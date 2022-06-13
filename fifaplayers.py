c3c76a3d29772b57e367fd76e5464c012f83675c
Jordan Rhinehart <103215302+KightGrizzly@users.noreply.github.com>
2022-06-13 12:42:33 -0400

    Rows and Columns shown

--- fifaplayers.py
+++ fifaplayers.py
@@ -7,11 +7,14 @@ df = pd.DataFrame(data, columns= ['short_name','nationality'])
 data2 = pd.read_csv (r'players_20.csv')
 df2 = pd.DataFrame(data2, columns= ['short_name','wage_eur'])
 
+for i in data2:
+    print(i, end= ', ')
 
-row_count = sum(1 for row in data2)
-column_count = sum(1 for column in data)
 
+row_count = data2.shape[1]
+column_count = data2.shape[0]
 
+print("\n")
 print (df)
 print("\n")
 print (df2)
