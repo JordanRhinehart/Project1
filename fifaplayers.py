b2639a8e70baa66986eb51943838a4e381a1839a
Jordan Rhinehart <103215302+KightGrizzly@users.noreply.github.com>
2022-06-13 12:17:39 -0400

    Added row count

--- fifaplayers.py
+++ fifaplayers.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import csv
 
-df = pd.read_csv (r'Path where the CSV file is stored\File name.csv')
+data = pd.read_csv (r'players_20.csv', nrows=5)
+df = pd.DataFrame(data, columns= ['short_name','nationality'])
+
+data2 = pd.read_csv (r'players_20.csv')
+df2 = pd.DataFrame(data2, columns= ['short_name','wage_eur'])
+
+
+row_count = sum(1 for row in data2)
+column_count = sum(1 for column in data)
+
+
 print (df)
-
+print("\n")
+print (df2)
+print("\n")
+print(f"The number of rows in this dataset are {row_count}")
+print(f"The number of columns in this dataset are {column_count}")
